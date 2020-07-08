71898a9cfaa0267847bf4a9ea253b2809006ea34
janya123 <67549506+janya123@users.noreply.github.com>
2020-07-08 21:01:42 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,40 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import Accordian from './components/Accordian';
 
-export default function App() {
-
-  const [option, setOption] = useState('');
-  const [location, setLocation] = useState('');
-  
-  const showFoodBanksHandler = (inputLocation, selectedOption) => {
-    setLocation(inputLocation);
-    setOption(selectedOption);
-  };
-
-  console.log(option);
-  console.log(location);
-
-  let content = <HomeScreen onStart={showFoodBanksHandler}/>;
-
-  if (option === 'donate') {
-    console.log('donate!');
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inventory :[
+        { 
+          title: 'Fruits and Vegetables',
+          data: 'Enter here',
+        },
+         { 
+          title: 'Grains',
+          data: 'Enter here',
+         },
+         { 
+          title: 'Legumes',
+          data: 'Enter here',
+         },
+         { 
+          title: 'Soups and Packaged Meals',
+          data: 'Enter here',
+         },
+         { 
+          title: 'Canned meat/fish',
+          data: 'Enter here',
+         },
+         { 
+          title: 'Snacks',
+          data: 'Enter here',
+         },
+         { 
+          title: 'Personal Hygiene Products',
+          data: 'Enter here',
+         },
+         { 
+          title: 'Cleaning Products',
+          data: 'Enter here',
+         },
+         { 
+          title: 'Specialty Items',
+          data: 'Enter here',
+         },
+      ]
+     }
   }
-  else if (option === 'find') {
-    console.log('find resources!');
+  render () {
+    return (
+      <View style={styles.container}>
+          <View style={styles.heading}>
+              <Text style={styles.textBank}>Bethel Mission Food Bank</Text>
+              <Text style={styles.textItem}>Items in Need: </Text>
+          </View>
+        { this.renderData() }
+      </View>
+    );
   }
-
-  return (
-    <View style={styles.screen}>
-      {content}
-    </View>
-  );
+  renderData=()=> {
+    const items = [];
+    for (item of this.state.inventory) {
+        items.push(
+            <Accordian 
+                title = {item.title}
+                data = {item.data}
+            />
+        );
+    }
+    return items;
+  }
 }
-
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    backgroundColor: '#65c1f0'
-  },
+  container: {
+   flex:1,
+   paddingTop:60,
+  }, 
+  heading: {
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  }, 
+  textBank: {
+    fontSize: 20,
+  }, 
+  textItem: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 5
+  }
 });
