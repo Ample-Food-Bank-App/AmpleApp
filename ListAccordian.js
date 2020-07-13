19204e1363a23c40c070190082fd8e88fe8a61ae
janya123 <67549506+janya123@users.noreply.github.com>
2020-07-14 01:08:17 +0530

    Update ListAccordian.js

--- ListAccordian.js
+++ ListAccordian.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, FlatList, LayoutAnimation, UIManager, Image} from "react-native";
+import { View, TouchableOpacity, Text, StyleSheet, FlatList, LayoutAnimation, UIManager, Image, YellowBox, Linking} from "react-native";
 
-export default class Accordian extends Component{
+YellowBox.ignoreWarnings(['VirtualizedLists should never be nested']);
+
+export default class Accordion extends Component{
     constructor(props) {
         super(props);
         this.state = {
             expanded: false,
-
-        }
+        };
         this.icons = {
             'up': require('../images/up.png'),
             'down' :require('../images/down.png')
@@ -25,36 +26,31 @@ export default class Accordian extends Component{
         if(this.state.expanded){
             icon = this.icons['up']; 
         }
-    
     return (
-        <View>
+        <View style={{backgroundColor: 'white', borderWidth: 0.5,
+        borderColor:'#8E8E93',  }}>
         <TouchableOpacity
           ref={this.accordion}
           style={styles.row}
           onPress={() => this.makeExpand()} 
         >
-        
-         <Text style={styles.textTitle}>{this.props.title}</Text>
-        
+          <Text style={styles.textTitle}>{this.props.title}</Text>
         <Image style = {styles.icon}
             source = {icon}
         ></Image>
-        
         </TouchableOpacity>
+
         {this.state.expanded && (
           <View style={styles.container}>
-            <FlatList
-            keyExtractor={item => item.key}
-            data={this.props.data}
+          <FlatList
             scrollEnabled={true}
-            renderItem={({ item, index }) => (
+            data={this.props.data}
+            renderItem={({ item }) => (
                 <View style={styles.item}>
-                    <View style={{ flex: 1 }}>
-                        <Text style={styles.textData}>{item.key}</Text>
-                    </View>
+                  <Text style={styles.textData}>{item.key}</Text>
                 </View>
               )}
-            />
+            ></FlatList>
           </View>
         )}
       </View>
@@ -64,34 +60,38 @@ export default class Accordian extends Component{
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    paddingHorizontal: 5,
+    paddingHorizontal: 2, 
   },
   item: {
-    padding: 2,
+    padding: 1,
     marginLeft: 25,
     marginRight: 15,
+
   },
   row: {
-    height: 37,
+    height: 40,
     paddingLeft: 25,
     paddingRight: 15,
-    paddingBottom: 0,
     margin: 5,
-    backgroundColor: "lightblue",
     justifyContent: "space-between",
     flexDirection: "row",
     alignItems: "center",
+    paddingBottom: 10,
   },
   textTitle: {
-    fontSize: 17,
+    fontSize: 18,
+    color: 'black',
   },
   textData: {
-    fontSize: 14,
+    fontSize: 15,
+    paddingBottom: 5,
+    paddingTop: 0,
   },
   icon: {
-    width: 27,
-    height: 27,
-    tintColor: "darkblue",
+    width: 25,
+    height: 25,
+    tintColor: '#3d3045',
+    alignItems: "center",
   },
   category: {
     alignItems: "flex-start",
