7cc38b61de7ce41251999312cc216841d4e85bdc
janya123 <67549506+janya123@users.noreply.github.com>
2020-07-17 03:14:34 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,71 +1,128 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
-import FoodBanksScreen from './screens/FoodBanksScreen';
-import DonateScreen from './screens/DonateScreen';
-import FindResourcesScreen from './screens/FindResourcesScreen';
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableWithoutFeedback} from 'react-native';
+import getDistance from 'geolib/es/getDistance';
+
+var myLong = 40.730610;
+var myLat = -73.935242;
+
+const FOODBANKS = [
+  { 
+    name: 'First United Methodist Church of Jamaica', 
+    longitude: 40.636190, 
+    latitude: -73.741430,
+  },
+  { 
+    name: 'Bethel Mission Food Bank', 
+    longitude: 40.695790, 
+    latitude: -73.799280, 
+  },
+] 
+
+const findDistance = () => {
+  for(var i=0; i<FOODBANKS.length; i++){
+    FOODBANKS[i]['distance'] = getDistance(
+      {latitude: myLat, longitude: myLong}, 
+      {latitude: JSON.stringify(FOODBANKS[i].latitude), longitude: JSON.stringify(FOODBANKS[i].longitude)}
+      ) * (0.00062137) ;
+  }
+}
+const compare = (a,b) => {
+  findDistance ();
+  if (a.distance < b.distance)
+     return -1;
+  if (a.distance > b.distance)
+    return 1;
+  return 0;
+}
+
+FOODBANKS.sort(compare);
 
 export default function App() {
 
-  const [option, setOption] = useState('');
-  const [location, setLocation] = useState('');
-  const [goToHome, setGoToHome] = useState(false);
-  const [goToList, setGoToList] = useState(false);
-  const [goToInventory, setGoToInventory] = useState(false);
-  
-  const showFoodBanksHandler = (inputLocation, selectedOption) => {
-    setLocation(inputLocation);
-    setOption(selectedOption);
-  };
-
-  const goToHomeHandler = goHome => {
-    setGoToHome(goHome);
-  };
-
-  const goToInventoryHandler = inventory => {
-    setGoToInventory(inventory);
-  };
-
-
-  const goToListingHandler = list => {
-    setGoToList(list);
-  }; 
-
-  let content = <HomeScreen onStart={showFoodBanksHandler} onGoToHome={goToHomeHandler}/>;
-
-  if (goToHome) {
-    content = <HomeScreen onStart={showFoodBanksHandler} onGoToHome={goToHomeHandler}/>;
-  }
-  else if ((option && location && !goToHome && !goToInventory) || goToList) {
-    content = <FoodBanksScreen 
-                onGoToHome={goToHomeHandler} 
-                locationOfUser={location} 
-                onGoToInv={goToInventoryHandler}
-                onGoToList={goToListingHandler}
-              />;
-  }
-  else if (goToInventory && option === 'donate' && !goToList){
-    content = <DonateScreen onGoToList={goToListingHandler}/>;
-  }
-  else if (goToInventory && option === 'find' && !goToList){
-    content = <FindResourcesScreen onGoToList={goToListingHandler}/>;
-  }
-
   return (
-    <View style={styles.screen}>
-      <StatusBar backgroundColor='#10518f' style={styles.bar}/>
-      {content}
+    <View style={styles.container}>
+      <Text style={styles.textBank}>Nearby Food Banks </Text>
+    <View style={styles.zip}>
+      <Text style={styles.textZip}>Zipcode: 02155</Text>
+    </View>
+      <View styles={{flex:1}}>
+      <FlatList
+          data={FOODBANKS}
+          renderItem= {( {item} ) => (
+            <View style ={{borderRadius: 15, borderWidth: 1, borderColor: 'white', marginBottom: 30, marginRight: 25,
+            marginLeft: 25, paddingTop:10, paddingBottom:30, backgroundColor: 'white'}}>
+              <Text style={styles.textName}>{item.name}</Text>
+              <Text style={styles.textData}>{item.distance.toFixed(1)} miles away</Text>
+              <Text style={styles.textMore}>See more</Text>
+            </View>
+          )}
+      >
+      </FlatList>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
+  container: {
     flex: 1,
-    backgroundColor: '#65c1f0'
+    width: '100%',
+    backgroundColor: '#E5E5EA',
   },
-  bar: {
-    marginBottom: 10
+  textZip: {
+    fontSize: 18,
+    color: 'white',
+    alignItems: 'center'
+  },
+  textName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingLeft: 25,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    paddingLeft: 15,
+    marginBottom: 10,
+    paddingRight: 15
+  },
+ textBank: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    paddingBottom: 10,
+    paddingTop: 80,
+    paddingLeft: 25,
+    paddingRight: 15,
+    color: 'white',
+    backgroundColor: '#3d3045',
+  },
+  textData: {
+    backgroundColor: '#3d3045',
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: 'white',
+    fontSize: 16,
+    color: '#fb8f67',
+    paddingBottom: 40,
+    marginBottom: 20,
+  },
+  zip: {
+    backgroundColor: '#fb8f67',  
+    alignItems: 'flex-end', 
+    paddingRight: 10, 
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginLeft:250,
+    marginBottom: 15,
+    marginRight: 15,
+  },
+  textMore: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderWidth: 0.75,
+    borderColor: '#3d3045',
+    fontSize: 14,
+    marginLeft: 10, 
+    marginRight: 250,
+    marginVertical: -30,
   }
 });
